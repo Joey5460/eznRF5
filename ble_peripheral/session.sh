93baa9ea3cce45ef51614c8f81880f63637b4ab6
zhouyu <zhouyu5460@gmail.com>
2017-03-09 11:16:49 +0800

    changed session.sh

--- ble_peripheral/session.sh
+++ ble_peripheral/session.sh
@@ -1,15 +1,17 @@
-if [[ $# -eq 0 ]] ; then
-	vim -S ~/.vim/sessions/balance.vim
-    exit 0
-fi
+#if [[ $# -eq 0 ]] ; then
+#	vim -S ~/.vim/sessions/balance.vim
+#    exit 0
+#fi
 
 while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
     le)
-		vim -S ~/.vim/sessions/zulp.vim
+		vim -S ~/.vim/sessions/ezle.vim
         ;;
+    b)
+        vim -S ~/.vim/sessions/balance.vim
     *)
         ;;
 esac
